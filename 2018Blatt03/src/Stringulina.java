f11d7ddd1527d460688b14b6ea214d9550d2ab3b
Liliya Jeromin <zl108108@gmail.com>
2019-05-26 23:20:11 +0200

    Update Stringulina.java

--- 2018Blatt03/src/Stringulina.java
+++ 2018Blatt03/src/Stringulina.java
@@ -34,12 +34,49 @@ public class Stringulina {
 		return count;
 	}
 
-	public static boolean correctlyBracketed(String str) {
-		return false;
-	}
+	  public static boolean correctlyBracketed(String str) {
+    int bracketCount = 0;
 
-	public static boolean matches(String str, String pattern) {
-		return true;
+    for (int i = 0; i < str.length(); i++) {
+      if (str.charAt(i) == '(')
+        bracketCount++;
+      else if (str.charAt(i) == ')') {
+        bracketCount--;
+        // Haben wir irgendwann mehr öffnende als schließende
+        // Klammern gesehen, ist der String nicht korrekt
+        // geklammert.
+        if (bracketCount < 0)
+          return false;
+      }
+    }
 
-	}
+    return bracketCount == 0;
+  }
+
+	 public static boolean matches(String str, String pattern) {
+    int posStr = 0;
+    int posPat = 0;
+    while (posPat < pattern.length()) {
+      char nextPat = pattern.charAt(posPat++);
+      int multiplicity = 1;
+      // Bei öffnender Klammer...
+      if (posPat < pattern.length() && pattern.charAt(posPat) == '{') {
+        // ... extrahieren wir die Vielfachheit
+        String multi_str = "";
+        for (int i = posPat + 1; pattern.charAt(i) != '}'; i++)
+          multi_str += pattern.charAt(i);
+        multiplicity = Integer.parseInt(multi_str);
+        // Wir überspringen die Vielfachheits-Angabe im Pattern
+        posPat += 2 + multi_str.length();
+      }
+      // Wir prüfen den Buchstaben aus dem Pattern und seine Vielfachheit auf
+      // Vorhandenheit im String
+      for (int i = 0; i < multiplicity; i++)
+        if (posStr >= str.length() || (str.charAt(posStr++) != nextPat && nextPat != '.'))
+          return false;
+    }
+
+    return posStr == str.length();
+  }
+}
 }
